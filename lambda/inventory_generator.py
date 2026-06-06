1d02667a57b95e676b1b75648d0fafb2ec6143a6
Adarsh-Hota <adarshhota55@gmail.com>
2026-06-07 02:36:28 +0530

    feat: Add inventory generator to create synthetic inventory event data for analytics

---
+++ lambda/inventory_generator.py
@@ -0,0 +1,73 @@
+import random
+import uuid
+
+import pandas as pd
+
+from datetime import datetime, timedelta
+
+
+MOVEMENT_TYPES = [
+    "STOCK_IN",
+    "STOCK_OUT",
+    "ADJUSTMENT"
+]
+
+WAREHOUSES = [
+    "WH_BLR",
+    "WH_MUM",
+    "WH_DEL",
+    "WH_HYD"
+]
+
+
+def generate_inventory_events(num_records=100):
+
+    inventory_events = []
+
+    for _ in range(num_records):
+
+        movement_type = random.choices(
+            MOVEMENT_TYPES,
+            weights=[30, 60, 10]
+        )[0]
+
+        if movement_type == "STOCK_IN":
+            quantity_change = random.randint(10, 200)
+
+        elif movement_type == "STOCK_OUT":
+            quantity_change = -random.randint(1, 20)
+
+        else:
+            quantity_change = random.randint(-10, 10)
+
+        inventory_event = {
+            "inventory_event_id": (
+                f"INV_{uuid.uuid4().hex[:8].upper()}"
+            ),
+            "product_id": (
+                f"PROD_{random.randint(100,999)}"
+            ),
+            "warehouse_id": random.choice(
+                WAREHOUSES
+            ),
+            "movement_type": movement_type,
+            "quantity_change": quantity_change,
+            "event_timestamp": (
+                datetime.now()
+                -
+                timedelta(
+                    hours=random.randint(
+                        1,
+                        720
+                    )
+                )
+            ).isoformat()
+        }
+
+        inventory_events.append(
+            inventory_event
+        )
+
+    return pd.DataFrame(
+        inventory_events
+    )

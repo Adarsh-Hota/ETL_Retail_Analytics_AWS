baf1e56dc615e2e697f59eac43fe656c830ea919
Adarsh-Hota <adarshhota55@gmail.com>
2026-07-02 23:59:55 +0530

    feat: Refactor order generation to include customer preferences and improve product selection logic for enhanced data realism

--- lambda/orders_generator.py
+++ lambda/orders_generator.py
@@ -5,7 +5,12 @@ import pandas as pd
 from datetime import datetime
 
 
-def generate_orders(customer_ids, product_lookup, num_records=100):
+def generate_orders(
+        customer_ids,
+        customer_lookup,
+        product_lookup,
+        num_records=100
+    ):
 
     orders = []
 
@@ -25,24 +30,51 @@ def generate_orders(customer_ids, product_lookup, num_records=100):
 
     for _ in range(num_records):
 
-        product_ids = list(product_lookup.keys())
+        customer_id = random.choice(customer_ids)
+
+        preferred_category = customer_lookup[customer_id]
+
+        preferred_products = []
+
+        other_products = []
+
+        for product_id, product_info in product_lookup.items():
+
+            if (product_info["category"]==preferred_category):
+                preferred_products.append(product_id)
+
+            else:
+                other_products.append(product_id)
+
+        if (preferred_products and random.random() < 0.7):
+            candidate_products = preferred_products
+
+        elif other_products:
+            candidate_products = other_products
+
+        else:
+            candidate_products = preferred_products
+
         weights = [
             product_lookup[p]["popularity_score"]
-            for p in product_ids
+            for p in candidate_products
         ]
+
         product_id = random.choices(
-            product_ids,
+            candidate_products,
             weights=weights,
             k=1
         )[0]
+
         quantity = random.randint(1, 5)
+
         unit_price = product_lookup[
             product_id
         ]["price"]
 
         order = {
             "order_id": str(uuid.uuid4()),
-            "customer_id": random.choice(customer_ids),
+            "customer_id": customer_id,
             "product_id": product_id,
             "quantity": quantity,
             "unit_price": unit_price,

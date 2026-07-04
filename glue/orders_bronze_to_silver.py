3a52b5e5ae5c058b5c770990bf47db608066d44b
Adarsh-Hota <adarshhota55@gmail.com>
2026-07-04 12:44:42 +0530

    feat: Enrich order data by joining with product details and casting order timestamp for improved data integrity

--- glue/orders_bronze_to_silver.py
+++ glue/orders_bronze_to_silver.py
@@ -28,6 +28,17 @@ df = spark.read.option("header", "true").csv(
     "s3://retail-analytics-platform/bronze/orders/"
 )
 
+products_df = spark.read.parquet(
+    "s3://retail-analytics-platform/silver/products/"
+)
+
+products_df = products_df.select(
+    "product_id",
+    "category",
+    "subcategory",
+    "brand"
+)
+
 df = (
     df
     .withColumn(
@@ -71,11 +82,24 @@ df_cleaned = (
         "processed_timestamp",
         current_timestamp()
     )
+    .withColumn(
+        "order_timestamp",
+        col("order_timestamp").cast(TimestampType())
+    )
+)
+
+
+df_enriched = (
+    df_cleaned.join(
+        products_df,
+        on="product_id",
+        how="left"
+    )
 )
 
 
 # Write parquet to silver layer
-df_cleaned.write \
+df_enriched.write \
     .mode("append") \
     .partitionBy("year", "month", "day") \
     .parquet(
